5dca35c0e0fc052f721846a0eda9239a59e7dd93
IliukhaDobrik <bogdobrinskiy@gmail.com>
2022-10-27 20:27:48 +0300

    LINQ homework is done

--- Homework_Module25/Homework_Module25/Program.cs
+++ Homework_Module25/Homework_Module25/Program.cs
@@ -1,3 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Bogus;
+using Homework_Module25.Enums;
+using System.Collections.Concurrent;
 
-Console.WriteLine("Hello, World!");
+namespace Homework_Module25
+{
+    public class Programm
+    {
+        public static void Main(string[] args)
+        {
+            var virtualDB = new VirtualDB();
+            virtualDB.CreateDB();
+
+            var persons = virtualDB.GetPersons();
+            var jobs = virtualDB.GetJobs();
+
+            var seacher = new Searcher();
+            var jobsForPerson = seacher.FullSearchByPerson(persons[0], jobs);
+            var personsForJob = seacher.FullSearchByJob(jobs[0], persons);
+
+            Console.WriteLine(persons[0]);
+            foreach (var job in jobsForPerson)
+            {
+                Console.WriteLine(job);
+            }
+
+            Console.WriteLine(jobs[0]);
+            foreach (var person in personsForJob)
+            {
+                Console.WriteLine(person);
+            }
+
+            seacher.WriteSuitableProfessions(persons, jobs);
+            seacher.WriteSexCount(persons);
+
+            Console.WriteLine();
+        }
+    }
+}
